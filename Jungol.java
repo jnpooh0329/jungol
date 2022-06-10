9ad1f4e4a15bc3dcbe1cd500da4a07189b27e102
jnpooh0329 <105191407+jnpooh0329@users.noreply.github.com>
2022-06-10 15:07:00 +0900

    1st commit

---
+++ Jungol.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Jungol {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int[]arr = new int[10];
+		int sum1 = 0;
+		double sum2 = 0;
+		
+		for(int i=0; i<arr.length; i++) {
+			arr[i] = sc.nextInt();
+			System.out.println("sum : " + even);
+			System.out.println((sum1 + sum2) / 2);
+		}
+	}
+}
